9d52a1fd8d7eb35e7fd9fdaa3b660bf2cfa1de5c
cookie_jar <cptpricecod098@gmail.com>
2024-06-15 20:00:03 +0530

    structs defined

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Block struct {
+	Pos      int
+	Data     ItemCheckout
+	Time     string
+	Hash     string
+	PrevHash string
+}
+
+type Item struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Seller string `json:"seller"`
+	Price  string `json:"price"`
+}
+
+type ItemCheckout struct {
+	ItemID    string `json:"item_id"`
+	Buyer     string `json:"buyer"`
+	Date      string `json:"date"`
+	IsGenesis bool   `json:"is_genesis"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var BlockChain *Blockchain
+
 func main() {
-	fmt.Println("hello")
 	r := mux.NewRouter()
-	r.HandleFunc("/").Methods("GET")
-	r.HandleFunc("/").Methods("POST")
+	r.HandleFunc("/", getBlocks).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
 	r.HandleFunc("/new", newItem).Methods("POST")
+
+	log.Println("Running on port: 3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 
 }
